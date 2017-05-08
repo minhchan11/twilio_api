0cbdb3d5f41cee2a9aca1fbf9650b0f222306086
Elise St Hilaire, Jiwon Kang and Minh Phuong <elise+jiwon+minh@>
2017-05-08 10:33:12 -0700

    add dependencies to project.json and create a model for Message object, also add a method that gets all messages

---
+++ src/Texter/Models/Message.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Texter.Models
+{
+    public class Message
+    {
+        public string To { get; set; }
+        public string From { get; set; }
+        public string Body { get; set; }
+        public string Status { get; set; }
+
+        public static List<Message> GetMessages()
+        {
+            var client = new RestClient("https://api.twilio.com/2010-04-01");
+
+            var request = new RestRequest("Accounts/{{Account SID}}/Messages.json", Method.GET);
+
+            client.Authenticator = new HttpBasicAuthenticator("{{Account SID}}", "{{Auth Token}}");
+
+            var response = new RestResponse();
+
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+
+            var messageList = JsonConvert.DeserializeObject<List<Message>>(jsonResponse["messages"].ToString());
+
+            return messageList;
+        }
+    }
+}
